81b21b69728b57a8eba110eb8af1d7bdeb9d8856
Hasina Akter <66660517+Hasina-Akter20@users.noreply.github.com>
2021-08-02 16:45:51 +0600

    first commit

---
+++ find-odd-or-even-number.js
@@ -0,0 +1,7 @@
+var number=32;
+if (number%2==0){
+    console.log("This is an even number.");
+}
+else{
+    console.log("This is an odd number");
+}
---
+++ sum-of-odds.js
@@ -0,0 +1,8 @@
+var sum=0;
+
+for (var i = 10; i <= 30; i++) {
+    if (i % 2 != 0) {      
+        sum+=i;
+    }
+}
+console.log(sum);
